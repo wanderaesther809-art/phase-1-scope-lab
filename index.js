3826d13065049c3be12a0d6520703b1a1cd99d75
Esther Wandera <wanderaesther809@gmail.com>
2025-09-22 22:47:06 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+
+    function upperCaseCustomerName(){
+        customerName = customerName.toUpperCase();
+
+    }
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+//the overwrite
+
+const leastFavoriteCustomer = "kirk";
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "Charlie";
+}
+
